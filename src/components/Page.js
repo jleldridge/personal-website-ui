25020bc9e6a2b93d943ce90b6f4013ca91a3aef8
Luke Eldridge <jleldridge27@gmail.com>
2020-04-21 22:26:26 -0700

    Make page headers smaller to better work with mobile.

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,7 +4,7 @@ class Page extends Component {
   render() {
     return (
       <div className="jumbotron jumbotron-fluid bg-light mt-3 pl-5 pr-5 shadow-sm rounded">
-        <h1 className="display-3 text-info">{this.props.title}</h1>
+        <h1 className="display-5 text-info">{this.props.title}</h1>
         {this.props.children}
       </div>
     );
