d5c0bc8078ec2500792b63dba51c179bc92f4c98
Luke Eldridge <jleldridge27@gmail.com>
2020-04-23 21:41:15 -0700

    Add admin login routes to express server.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
+var router = express.Router();
 const rootDir = process.cwd();
 
-app.use(express.static(path.join(rootDir, 'build')));
+// app.get("/", function (req, res) {
+//   console.log(req);
+//   res.sendFile(path.join(rootDir, "build", "index.html"));
+// });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(rootDir, 'build', 'index.html'));
+app.get("/admin/login", function (req, res) {
+  res.sendFile(path.join(rootDir, "server", "views", "login.html"));
 });
 
+app.post("/admin/login", function (req, res) {
+  console.log(req);
+  res.send("Hello admin!");
+});
+
+app.use(require(path.join(rootDir, "server", "error-handler")));
+app.use(express.static(path.join(rootDir, "build")));
+
+console.log("server started!");
 app.listen(9000);
