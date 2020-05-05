4da5d2b14db714940240681d3be3c5d04dfc6bbe
Jeffrey Lucas Eldridge <jleldridge27@gmail.com>
2020-05-05 10:24:46 -0700

    Typos in comments.

--- server/redisClient.js
+++ server/redisClient.js
@@ -5,12 +5,12 @@ const redisClient = redis.createClient({
   retry_strategy: function (options) {
     if (options.error && options.error.code === "ECONNREFUSED") {
       // End reconnecting on a specific error and flush all commands with
-      // a individual error
+      // an individual error
       return new Error("The server refused the connection");
     }
     if (options.total_retry_time > 1000 * 60 * 60) {
       // End reconnecting after a specific timeout and flush all commands
-      // with a individual error
+      // with an individual error
       return new Error("Retry time exhausted");
     }
     if (options.attempt > 10) {
