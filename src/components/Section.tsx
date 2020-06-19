85351ebf9bdb357fe0ca77829efd518cb7dc0cc8
Luke Eldridge <jleldridge27@gmail.com>
2020-06-19 15:51:36 -0700

    Remove console logs.

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -21,11 +21,10 @@ export default function Section(props: Props) {
     axios
       .get(config.API_SERVER_URL + `/content/${props.sectionName}`)
       .then(function (response) {
-        console.log(response);
         dispatch(storeContent(props.sectionName, response.data));
       })
       .catch(function (error) {
-        console.log(error);
+        console.error(error);
       });
   }, [props.sectionName]);
 
